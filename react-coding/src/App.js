9e72dfb54c4c506c753935cbc73989a1cce415a8
Besir Gunduz <besirgunduz1993@gmail.com>
2021-10-30 22:27:57 +0300

    array states

--- react-coding/src/App.js
+++ react-coding/src/App.js
@@ -3,44 +3,30 @@ import Header from "./components/Header";
 import User from "./components/User";
 import { useState } from "react";
 
-const friends = [
-  {
-    id: 1,
-    name: "Rıdvan",
-  },
-  {
-    id: 2,
-    name: "Eshat",
-  },
-  {
-    id: 3,
-    name: "Zeyni",
-  },
-];
-
 function App() {
   const [name, setName] = useState("Beşir");
   const [age, setAge] = useState(28);
-
-  console.log(name, age);
+  const [friends, setFriends] = useState(["Beşir", "Rıza", "Ahmet"]);
 
   return (
     <div className="App">
-      <Header />
-
       <h1>Benim adım {name}</h1>
       <h2>Yaşım {age}</h2>
 
       <button onClick={() => setName("Rıza")}>Click</button>
       <button onClick={() => setAge(25)}>Click2</button>
 
-      <User
-        name="Beşir"
-        surname="Gündüz"
-        isLoggedIn={true}
-        age={28}
-        friends={friends}
-      />
+      <hr />
+      <br />
+
+      <h1>Friends</h1>
+      {friends.map((friend, index) => (
+        <div key={index}>{friend}</div>
+      ))}
+
+      <button onClick={() => setFriends([...friends, "Ayşe"])}>
+        Add new friend
+      </button>
     </div>
   );
 }
