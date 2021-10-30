f4413fca4297ec955511dafe3896a80d5376befd
Besir Gunduz <besirgunduz1993@gmail.com>
2021-10-30 22:04:34 +0300

    state

--- react-coding/src/App.js
+++ react-coding/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Header from "./components/Header";
 import User from "./components/User";
+import { useState } from "react";
 
 const friends = [
   {
@@ -18,9 +19,21 @@ const friends = [
 ];
 
 function App() {
+  const [name, setName] = useState("Beşir");
+  const [age, setAge] = useState(28);
+
+  console.log(name, age);
+
   return (
     <div className="App">
       <Header />
+
+      <h1>Benim adım {name}</h1>
+      <h2>Yaşım {age}</h2>
+
+      <button onClick={() => setName("Rıza")}>Click</button>
+      <button onClick={() => setAge(25)}>Click2</button>
+
       <User
         name="Beşir"
         surname="Gündüz"
