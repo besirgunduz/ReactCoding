baf952ce2bc0de5108109d0887f06f29ced61909
Besir Gunduz <besirgunduz1993@gmail.com>
2021-11-02 22:16:10 +0300

    added axios

--- react-coding/src/components/Users.jsx
+++ react-coding/src/components/Users.jsx
@@ -1,13 +1,19 @@
 import { useEffect, useState } from "react";
+import axios from "axios";
 
 function Users() {
   const [users, setUsers] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((response) => response.json())
-      .then((data) => setUsers(data))
+    // fetch("https://jsonplaceholder.typicode.com/users")
+    //   .then((response) => response.json())
+    //   .then((data) => setUsers(data))
+    //   .catch((e) => console.log(e))
+    //   .finally(() => setIsLoading(false));
+
+    axios("https://jsonplaceholder.typicode.com/users")
+      .then((response) => setUsers(response.data))
       .catch((e) => console.log(e))
       .finally(() => setIsLoading(false));
   }, []);
