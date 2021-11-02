213ad55cb670d384db5275f406c192017d04f8c9
Besir Gunduz <besirgunduz1993@gmail.com>
2021-11-02 22:06:10 +0300

    native fetch

--- react-coding/src/App.js
+++ react-coding/src/App.js
@@ -1,78 +1,11 @@
 import "./App.css";
-import { useState, useEffect } from "react";
-import Counter from "./components/Counter";
-import InputExample from "./components/InputExample";
+import Users from "./components/Users";
 
 function App() {
-  const [name, setName] = useState("Beşir");
-  const [age, setAge] = useState(28);
-  const [friends, setFriends] = useState(["Beşir", "Rıza", "Ahmet"]);
-  const [address, setAddress] = useState({ title: "Istanbul", zip: 34290 });
-  const [number, setNumber] = useState(0);
-
-  useEffect(() => {
-    console.log("Component mount edildi");
-  },[]);
-
-  //sadece number state dinler
-  useEffect(() => {
-    console.log("number state güncellendi");
-  }, [number]);
-
   return (
     <div className="App">
-      <h1>Benim adım {name}</h1>
-      <h2>Yaşım {age}</h2>
-
-      <button onClick={() => setName("Rıza")}>Click</button>
-      <button onClick={() => setAge(25)}>Click2</button>
-
-      <hr />
-      <br />
-
-      <h1>Friends</h1>
-      {friends.map((friend, index) => (
-        <div key={index}>{friend}</div>
-      ))}
-
-      <button onClick={() => setFriends([...friends, "Ayşe"])}>
-        Add new friend
-      </button>
-
-      <hr />
-      <br />
-
-      <h1>Address</h1>
-      <div>
-        {address.title} {address.zip}
-      </div>
-
-      <button
-        onClick={() => setAddress({ ...address, title: "Ankara", zip: 232526 })}
-      >
-        change the address
-      </button>
-
-      <Counter />
-      <InputExample />
-
-      <hr />
-      <h1>{number}</h1>
-
-      <button onClick={() => setNumber(number + 1)}>Click</button>
-      <hr />
-
-      <div style={{color:"red", backgroundColor:"white", padding:20}}>
-        Lorem Ipsum, dizgi ve baskı endüstrisinde kullanılan mıgır metinlerdir.
-        Lorem Ipsum, adı bilinmeyen bir matbaacının bir hurufat numune kitabı
-        oluşturmak üzere bir yazı galerisini alarak karıştırdığı 1500'lerden
-        beri endüstri standardı sahte metinler olarak kullanılmıştır. Beşyüz yıl
-        boyunca varlığını sürdürmekle kalmamış, aynı zamanda pek değişmeden
-        elektronik dizgiye de sıçramıştır. 1960'larda Lorem Ipsum pasajları da
-        içeren Letraset yapraklarının yayınlanması ile ve yakın zamanda Aldus
-        PageMaker gibi Lorem Ipsum sürümleri içeren masaüstü yayıncılık
-        yazılımları ile popüler olmuştur.
-      </div>
+      <h1>Merhaba Dünya</h1>
+      <Users />
     </div>
   );
 }
---
+++ react-coding/src/components/Users.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+
+function Users() {
+  const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((data) => setUsers(data))
+      .catch((e) => console.log(e))
+      .finally(() => setIsLoading(false));
+  }, []);
+
+  return (
+    <div>
+      {isLoading && <div>Loading...</div>}
+      {users.map((user) => (
+        <div key={user.id}>{user.name}</div>
+      ))}
+    </div>
+  );
+}
+
+export default Users;
